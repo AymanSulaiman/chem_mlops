1a264580940045fde0583c3dda8b60eaf2c98659
AymanSulaiman <saymansulaiman@gmail.com>
2025-12-22 21:53:42 -0800

    starting prompt again

--- app/orchestration/data_transformation.py
+++ app/orchestration/data_transformation.py
@@ -1,8 +1,11 @@
 from prefect import flow, task
 from app.scripts.flows.initial_data_transformation.collect_data import collect_data
 from app.scripts.flows.initial_data_transformation.transform_data import transform_data
-from app.scripts.flows.llm_finetuning_data.build_finetune_dataset import create_finetuning_dataset
+from app.scripts.flows.llm_finetuning_data.build_finetune_dataset import (
+    create_finetuning_dataset,
+)
 from app.scripts.flows.finetuning.finetuning import gemma3_chembl_toon_finetune_flow
+
 
 @task
 def collect_data_task() -> None:
@@ -13,9 +16,11 @@ def collect_data_task() -> None:
 def transform_data_task() -> None:
     transform_data()
 
+
 @task
 def create_finetune_dataset_task() -> None:
     create_finetuning_dataset()
+
 
 @task
 def finetune_llm_task() -> None:
@@ -28,6 +33,7 @@ def chembl_pipeline() -> None:
     transform_data_task()
     create_finetune_dataset_task()
     gemma3_chembl_toon_finetune_flow()
+
 
 if __name__ == "__main__":
     chembl_pipeline()
